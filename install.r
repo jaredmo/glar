3f3ac62a0cfb7ce65d974a623b1f2073f0b46a5b
Jared <github@monger.cc>
2017-06-25 09:54:44 -0500

    Daily changes

---
+++ install.r
@@ -0,0 +1,12 @@
+# =====================================================================
+# File:            install.r
+# Title:           General Ledger Analytics for R
+# Author:          Jared Monger
+# Create date:     6/24/17
+# Input(s):        
+# Description:     Install required packages
+# =====================================================================
+
+install.packages("ggplot2")
+install.packages("readr")
+install.packages("data.table")
